adbb1e9bed0259e8ac113afbcc93def4332a9787
Mwamtindi <mwamtindishaban@gmail.com>
2024-08-10 12:55:14 +0300

    implement auth,appwrite, and sentry

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -5,6 +5,7 @@ import { cn } from '@/lib/utils'
 import Image from 'next/image'
 import Link from 'next/link'
 import { usePathname } from 'next/navigation'
+import Footer from './Footer'
 
 const Sidebar = ({ user }: SiderbarProps) => {
   const pathname = usePathname();
@@ -45,7 +46,11 @@ const Sidebar = ({ user }: SiderbarProps) => {
               </Link>
             )
          })}
+
+         USER
        </nav>
+
+       <Footer user={user} />
     </section>
   )
 }
---
+++ lib/actions/user.actions.ts
@@ -0,0 +1,95 @@
+'use server';
+
+import { ID } from "node-appwrite";
+import { createAdminClient, createSessionClient } from "../appwrite";
+import { cookies } from "next/headers";
+import { parseStringify } from "../utils";
+
+export const signIn = async ({ email, password }: signInProps) => {
+    try {
+      const { account } = await createAdminClient();
+
+      const response = await account.createEmailPasswordSession(email, password);
+
+      return parseStringify(response);
+    } catch (error) {
+      console.error('Error', error);  
+    }
+}
+
+export const signUp = async (userData: SignUpParams) => {
+    const { email, password, firstName, lastName } = userData;
+
+    try {
+      const { account, } = await createAdminClient();
+
+      const newUserAccount = await account.create(
+        ID.unique(),
+        email,
+        password,
+        `${firstName} ${lastName}`
+      );
+
+      {/*if(!newUserAccount) throw new Error('Error creating user')
+
+        const dwollaCustomerUrl = await createDwollaCustomer({
+          ...userData,
+          type: 'personal'
+        })
+    
+        if(!dwollaCustomerUrl) throw new Error('Error creating Dwolla customer')
+    
+        const dwollaCustomerId = extractCustomerIdFromUrl(dwollaCustomerUrl);
+    
+        const newUser = await database.createDocument(
+          DATABASE_ID!,
+          USER_COLLECTION_ID!,
+          ID.unique(),
+          {
+            ...userData,
+            userId: newUserAccount.$id,
+            dwollaCustomerId,
+            dwollaCustomerUrl
+          }
+        )*/}
+        const session = await account.createEmailPasswordSession(email, password);
+
+        cookies().set("appwrite-session", session.secret, {
+          path: "/",
+          httpOnly: true,
+          sameSite: "strict",
+          secure: true,
+        });
+
+        return parseStringify(newUserAccount);
+      } catch (error) {
+        console.error('Error', error);
+    }
+}
+
+export async function getLoggedInUser() {
+  try {
+    const { account } = await createSessionClient();
+    const user = await account.get();
+    //const result = await account.get();
+
+    //const user = await getUserInfo({ userId: result.$id})
+
+    return parseStringify(user);
+  } catch (error) {
+    console.log(error)
+    return null;
+  }
+}
+
+export const logoutAccount = async () => {
+  try {
+    const { account } = await createSessionClient();
+    
+    cookies().delete('appwrite-session');
+
+    await account.deleteSession('current');
+  } catch (error) {
+    return null;
+  }
+}
